36e1711083bd780fe27de71ce94916451705817b
Jose Cruz <josealfredo8298669242@gmail.com>
2025-06-17 23:21:12 -0400

    Primer Api Rest con nodejs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import productsRoutes from './routes/products.routes.js';
+import cors from 'cors';
 
 // Importamos el Router de Express
 const app = express();
-
+app.use(cors()); // Middleware para habilitar CORS
 app.use(express.json()); // Middleware para parsear JSON
 
 app.use(productsRoutes); // Usamos las rutas de productos
